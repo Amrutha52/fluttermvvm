f9489a9847bef3f40cc33ea5f5f7fd75fe149bae
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-09-11 18:16:23 +0530

    first commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget
+{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text("Home"),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.search)),
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.settings))
+        ],
+        backgroundColor: Colors.greenAccent,
+        elevation: 20,
+      ),
+    );
+  }
+}

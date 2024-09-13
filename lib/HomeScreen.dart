f9f1d8515dea5e01f4f319ec488619751ddf6c93
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-09-13 10:57:53 +0530

    TextField is implemented

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -11,6 +11,8 @@ class HomeScreen extends StatefulWidget
 
 class _HomeScreenState extends State<HomeScreen>
 {
+  TextEditingController userNameControlller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,30 +30,56 @@ class _HomeScreenState extends State<HomeScreen>
         backgroundColor: Colors.greenAccent,
         elevation: 20,
       ),
-      body:
-      SingleChildScrollView(
-        scrollDirection: Axis.vertical,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          
           children: [
-            Container(
-              color: Colors.red,
-              height: 100,
-              width: 500,
+            TextField(
+              keyboardType: TextInputType.number,// to select keyboard type
+              obscureText: true, // masking for password
+              controller: userNameControlller,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                      hintText: "Enter UserName",
+                labelText: "User Name",
+               prefixIcon: Icon(Icons.lock),
+               suffixIcon: Icon(Icons.toggle_off),
+
+               // icon: Icon(Icons.favorite) // to display outside the box
+              ),
+
             ),
-            Container(
-              color: Colors.green,
-              height: 100,
-              width: 500,
-            ),
-            Container(
-              color: Colors.blueGrey,
-              height: 100,
-              width: 500,
-            )
+            ElevatedButton(
+                onPressed: (){
+                  print(userNameControlller.text);
+                },
+                child: Text("Login"))
           ],
         ),
-      ),
+      )
+      // SingleChildScrollView(
+      //   scrollDirection: Axis.vertical,
+      //   child: Column(
+      //
+      //     children: [
+      //       Container(
+      //         color: Colors.red,
+      //         height: 100,
+      //         width: 500,
+      //       ),
+      //       Container(
+      //         color: Colors.green,
+      //         height: 100,
+      //         width: 500,
+      //       ),
+      //       Container(
+      //         color: Colors.blueGrey,
+      //         height: 100,
+      //         width: 500,
+      //       )
+      //     ],
+      //   ),
+      // ),
       // ListView(
       //   scrollDirection: Axis.horizontal,
       //   children: [
